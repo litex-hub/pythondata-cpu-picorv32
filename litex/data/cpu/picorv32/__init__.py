f8deed2e325caf8d271f4e495f3b484e8b236315
LiteX <robot@timvideos.us>
2020-04-06 15:27:54 +0000

    Updating module to 1.0.post66
    Updated data to v1.0-23-ge308982 based on e308982e18fc952a8d446ddb7ea8b70433a998c2 from https://github.com/cliffordwolf/picorv32.
    > commit e308982e18fc952a8d446ddb7ea8b70433a998c2
    > Merge: 46aa89c 1e24e99
    > Author: Clifford Wolf <clifford@clifford.at>
    > Date:   Mon Nov 18 14:21:10 2019 +0100
    >
    >     Merge pull request #141 from rxrbln/master
    >
    >     added CROSS prefix and CFLAGS to the picsoc/Makefile
    >
    
    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/picorv32/__init__.py
+++ litex/data/cpu/picorv32/__init__.py
@@ -2,12 +2,43 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/cliffordwolf/picorv32"
-git_hash = "e308982e18fc952a8d446ddb7ea8b70433a998c2"
-git_describe = "v1.0-23-ge308982"
-version_str = "1.0.post23"
-version_tuple = (1, 0)
+
+# Module version
+version_str = "1.0.post66"
+version_tuple = (1, 0, 66)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post23")
+    pversion = V("1.0.post66")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "1.0.post23"
+data_version_tuple = (1, 0, 23)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("1.0.post23")
+except ImportError:
+    pass
+data_git_hash = "e308982e18fc952a8d446ddb7ea8b70433a998c2"
+data_git_describe = "v1.0-23-ge308982"
+data_git_msg = """\
+commit e308982e18fc952a8d446ddb7ea8b70433a998c2
+Merge: 46aa89c 1e24e99
+Author: Clifford Wolf <clifford@clifford.at>
+Date:   Mon Nov 18 14:21:10 2019 +0100
+
+    Merge pull request #141 from rxrbln/master
+    
+    added CROSS prefix and CFLAGS to the picsoc/Makefile
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post43"
+tool_version_tuple = (0, 0, 43)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post43")
 except ImportError:
     pass
--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import picorv32
 
-print("Found picorv32 @ version", picorv32.version_str, "("+picorv32.git_hash+")")
+print("Found picorv32 @ version", picorv32.version_str, "(with data", picorv32.data_version_str, ")")
+print()
 print("Data is in", picorv32.data_location)
 assert os.path.exists(picorv32.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(picorv32.data_location)))
+print("Data is version", picorv32.data_version_str, picorv32.data_git_hash)
+print("-"*75)
+print(picorv32.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(picorv32.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), picorv32.data_location)
+        print(" -", path)
