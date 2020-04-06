ff642e6ef163f3081bd352b325ef3b07ddae2db1
LiteX <robot@timvideos.us>
2020-04-06 18:20:27 +0000

    Updating module to 1.0.post69
    Updated data to v1.0-23-ge308982 based on e308982e18fc952a8d446ddb7ea8b70433a998c2 from https://github.com/cliffordwolf/picorv32.
    > commit e308982e18fc952a8d446ddb7ea8b70433a998c2
    > Merge: 46aa89c 1e24e99
    > Author: Clifford Wolf <clifford@clifford.at>
    > Date:   Mon Nov 18 14:21:10 2019 +0100
    >
    >     Merge pull request #141 from rxrbln/master
    >
    >     added CROSS prefix and CFLAGS to the picsoc/Makefile
    >
    
    Updated using 0.0.post46 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/picorv32/__init__.py
+++ litex/data/cpu/picorv32/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/cliffordwolf/picorv32"
 
 # Module version
-version_str = "1.0.post66"
-version_tuple = (1, 0, 66)
+version_str = "1.0.post69"
+version_tuple = (1, 0, 69)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post66")
+    pversion = V("1.0.post69")
 except ImportError:
     pass
 
@@ -35,10 +35,10 @@ Date:   Mon Nov 18 14:21:10 2019 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post43"
-tool_version_tuple = (0, 0, 43)
+tool_version_str = "0.0.post46"
+tool_version_tuple = (0, 0, 46)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post43")
+    ptool_version = V("0.0.post46")
 except ImportError:
     pass
