8b5024d0fe2bfdc5c59588934338c79d040160c7
LiteX <robot@timvideos.us>
2020-04-15 17:47:55 +0000

    Updating pythondata-cpu-picorv32 to 1.0.post74
    Updated data to v1.0-25-g824a5c8 based on 824a5c801194299f89f4fd8844c1fbcd1bdf4e21 from https://github.com/cliffordwolf/picorv32.
    > commit 824a5c801194299f89f4fd8844c1fbcd1bdf4e21
    > Merge: e308982 a7ff70d
    > Author: Claire Wolf <clifford@clifford.at>
    > Date:   Wed Apr 15 18:49:23 2020 +0200
    >
    >     Merge pull request #158 from rxrbln/uart
    >
    >     added default clk divider parameter to simpleuart
    >
    
    Updated using 0.0.post49 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_picorv32/__init__.py
+++ pythondata_cpu_picorv32/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/cliffordwolf/picorv32"
 
 # Module version
-version_str = "1.0.post72"
-version_tuple = (1, 0, 72)
+version_str = "1.0.post74"
+version_tuple = (1, 0, 74)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post72")
+    pversion = V("1.0.post74")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post23"
-data_version_tuple = (1, 0, 23)
+data_version_str = "1.0.post25"
+data_version_tuple = (1, 0, 25)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post23")
+    pdata_version = V("1.0.post25")
 except ImportError:
     pass
-data_git_hash = "e308982e18fc952a8d446ddb7ea8b70433a998c2"
-data_git_describe = "v1.0-23-ge308982"
+data_git_hash = "824a5c801194299f89f4fd8844c1fbcd1bdf4e21"
+data_git_describe = "v1.0-25-g824a5c8"
 data_git_msg = """\
-commit e308982e18fc952a8d446ddb7ea8b70433a998c2
-Merge: 46aa89c 1e24e99
-Author: Clifford Wolf <clifford@clifford.at>
-Date:   Mon Nov 18 14:21:10 2019 +0100
+commit 824a5c801194299f89f4fd8844c1fbcd1bdf4e21
+Merge: e308982 a7ff70d
+Author: Claire Wolf <clifford@clifford.at>
+Date:   Wed Apr 15 18:49:23 2020 +0200
 
-    Merge pull request #141 from rxrbln/master
+    Merge pull request #158 from rxrbln/uart
     
-    added CROSS prefix and CFLAGS to the picsoc/Makefile
+    added default clk divider parameter to simpleuart
 
 """
 
