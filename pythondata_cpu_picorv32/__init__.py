8b1bb2cc173df7a8e80118cd52b23019364ad11f
LiteX Robot <robot@timvideos.us>
2021-02-15 16:42:37 +0000

    Updating pythondata-cpu-picorv32 to 1.0.post129
    Updated data to v1.0-39-gf9b1beb based on f9b1beb4cfd6b382157b54bc8f38c61d5ae7d785 from https://github.com/cliffordwolf/picorv32.
    > commit f9b1beb4cfd6b382157b54bc8f38c61d5ae7d785
    > Author: Larry Doolittle <ldoolitt@recycle.lbl.gov>
    > Date:   Mon Apr 27 17:23:45 2020 +0200
    >
    >     Make Makefile more flexible
    >
    >     Signed-off-by: Claire Wolf <claire@symbioticeda.com>
    >
    
    Updated using 0.0.post90 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_picorv32/__init__.py
+++ pythondata_cpu_picorv32/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/cliffordwolf/picorv32"
 
 # Module version
-version_str = "1.0.post128"
-version_tuple = (1, 0, 128)
+version_str = "1.0.post129"
+version_tuple = (1, 0, 129)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post128")
+    pversion = V("1.0.post129")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Mon Apr 27 17:23:45 2020 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post89"
-tool_version_tuple = (0, 0, 89)
+tool_version_str = "0.0.post90"
+tool_version_tuple = (0, 0, 90)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post89")
+    ptool_version = V("0.0.post90")
 except ImportError:
     pass
 
