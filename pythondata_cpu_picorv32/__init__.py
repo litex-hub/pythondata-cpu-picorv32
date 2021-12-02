0c327bcc48269ab44cc342b1368af065dd8b1823
LiteX Robot <robot@timvideos.us>
2021-12-02 16:03:08 +0000

    Updating pythondata-cpu-picorv32 to 1.0.post152
    Updated data to v1.0-40-g100e421 based on 100e421be0aa5fb130b6e4f532d15fc7d1a03f02 from https://github.com/cliffordwolf/picorv32.
    > commit 100e421be0aa5fb130b6e4f532d15fc7d1a03f02
    > Author: Claire Xenia Wolf <claire@clairexen.net>
    > Date:   Thu Dec 2 15:59:12 2021 +0100
    >
    >     Fix copyright info
    >
    >     Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_picorv32/__init__.py
+++ pythondata_cpu_picorv32/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/cliffordwolf/picorv32"
 
 # Module version
-version_str = "1.0.post151"
-version_tuple = (1, 0, 151)
+version_str = "1.0.post152"
+version_tuple = (1, 0, 152)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post151")
+    pversion = V("1.0.post152")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post39"
-data_version_tuple = (1, 0, 39)
+data_version_str = "1.0.post40"
+data_version_tuple = (1, 0, 40)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post39")
+    pdata_version = V("1.0.post40")
 except ImportError:
     pass
-data_git_hash = "f9b1beb4cfd6b382157b54bc8f38c61d5ae7d785"
-data_git_describe = "v1.0-39-gf9b1beb"
+data_git_hash = "100e421be0aa5fb130b6e4f532d15fc7d1a03f02"
+data_git_describe = "v1.0-40-g100e421"
 data_git_msg = """\
-commit f9b1beb4cfd6b382157b54bc8f38c61d5ae7d785
-Author: Larry Doolittle <ldoolitt@recycle.lbl.gov>
-Date:   Mon Apr 27 17:23:45 2020 +0200
+commit 100e421be0aa5fb130b6e4f532d15fc7d1a03f02
+Author: Claire Xenia Wolf <claire@clairexen.net>
+Date:   Thu Dec 2 15:59:12 2021 +0100
 
-    Make Makefile more flexible
+    Fix copyright info
     
-    Signed-off-by: Claire Wolf <claire@symbioticeda.com>
+    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>
 
 """
 
